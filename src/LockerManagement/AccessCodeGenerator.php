623e3de2bf7c028d1ca10a9c1e081d08cd064288
Hugo Hamon <hugohamon@neuf.fr>
2018-01-26 13:32:01 +0100

    Use random_bytes() instead of uniqid() function

--- src/LockerManagement/AccessCodeGenerator.php
+++ src/LockerManagement/AccessCodeGenerator.php
@@ -6,6 +6,6 @@ class AccessCodeGenerator
 {
     public function generate(int $length = 7): string
     {
-        return substr(sha1(uniqid()), 0, $length);
+        return substr(sha1(random_bytes(32)), 0, $length);
     }
 }
